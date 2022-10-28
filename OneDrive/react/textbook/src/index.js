d904f0971c5a6203a670a27b293419c71d6b7439
rhrh9999 <osht9499@naver.com>
2022-10-28 18:12:46 +0900

    textbook\src\index.js

---
+++ OneDrive/react/textbook/src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import PassingCount from"./chapter_12/PassingCount.jsx";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+<div> <PassingCount /></div>
+);
