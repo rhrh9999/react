3314c22131ea724b7fada8efcdb5cb9d59282632
rhrh9999 <112358232+rhrh9999@users.noreply.github.com>
2022-10-30 04:05:10 +0900

    Delete index.js

--- OneDrive/react/textbook/src/chapter_12/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import PassingCount from"./chapter_12/PassingCount.jsx";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-<div> <PassingCount /></div>
-);
